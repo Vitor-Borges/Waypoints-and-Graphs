eecd88216f2cfb1e386365c265ae3debc4ac60cf
Vitor-Borges <62852982+Vitor-Borges@users.noreply.github.com>
2022-12-26 22:46:10 +0000

    Following a Circuit
    Epata concluida.

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -9,20 +9,38 @@ public class FollowWP : MonoBehaviour
 
         public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3) currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 20) * Time.deltaTime);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 10) currentWP++;
+        ProgressTracker();
 
-        if (currentWP >= waypoints.Length)
-            currentWP = 0;
-
-        // this.transform.LookAt(waypoints[currentWP].transform);
+        
 
         Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
 
